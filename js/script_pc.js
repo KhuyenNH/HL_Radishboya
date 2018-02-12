087838b94164043d509a1eb5027f112e5b8d6ae9
KhuyenNH <huukhuyen.dev@gmail.com>
2018-02-12 16:48:59 +0700

    update

--- js/script_pc.js
+++ js/script_pc.js
@@ -15,3 +15,9 @@ $(function(){
 	imgChange();
 	smoothScroll();
 });
+
+$('img').hover(function() {
+	$(this).attr("src", $(this).attr("src").replace('_off', '_on'));
+}, function() {
+	$(this).attr("src", $(this).attr("src").replace('_on', '_off'));
+});
